4178bb0acba1fd9ea857345fe0c9b99bf8a135a1
ring <trinhrinh55@gmail.com>
2022-12-09 23:14:51 +0700

    last

---
+++ client/build/precache-manifest.121fc18d648e67429739e272427fdba4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7ce8cd69d4c0ea9fdab75a8a01eb65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e578f32f48e263c98ea4",
+    "url": "/static/css/2.70701490.chunk.css"
+  },
+  {
+    "revision": "d1c845ff98c88160992b",
+    "url": "/static/css/main.4519887b.chunk.css"
+  },
+  {
+    "revision": "e578f32f48e263c98ea4",
+    "url": "/static/js/2.aa3995ba.chunk.js"
+  },
+  {
+    "revision": "a48dff562bc42bb429fa7742e719bae2",
+    "url": "/static/js/2.aa3995ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1c845ff98c88160992b",
+    "url": "/static/js/main.33cbd417.chunk.js"
+  },
+  {
+    "revision": "57e402ecafc0c10bdab4",
+    "url": "/static/js/runtime-main.adafc510.js"
+  },
+  {
+    "revision": "41dd1c05cc145ecc51b6e692121ad2c1",
+    "url": "/static/media/img_effect1.41dd1c05.png"
+  },
+  {
+    "revision": "eef2d43581d9a5ce85a900d4edc231af",
+    "url": "/static/media/img_effect2.eef2d435.png"
+  },
+  {
+    "revision": "116c4b14d916c30a7e94126221b2c837",
+    "url": "/static/media/img_effect31.116c4b14.png"
+  },
+  {
+    "revision": "353365bea8eead8156249d0db7242a70",
+    "url": "/static/media/img_effect32.353365be.png"
+  },
+  {
+    "revision": "3f2590067056b4f0630d5b360e694fce",
+    "url": "/static/media/loading.3f259006.gif"
+  },
+  {
+    "revision": "718dc4d5532f6331308aa8c875f80368",
+    "url": "/static/media/logo.718dc4d5.png"
+  },
+  {
+    "revision": "a91729cee85d0b9163a882649bbece36",
+    "url": "/static/media/slide1.a91729ce.png"
+  },
+  {
+    "revision": "f2b7d37f5e57e3d2a14dfc70dd88329f",
+    "url": "/static/media/slide2.f2b7d37f.png"
+  },
+  {
+    "revision": "964837beb2d900a1e14f64b1dd7061f3",
+    "url": "/static/media/slide3.964837be.png"
+  },
+  {
+    "revision": "ce905ec3ea13af084a19f3fef9e1c75f",
+    "url": "/static/media/slide4.ce905ec3.png"
+  }
+]);
